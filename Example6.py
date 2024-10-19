403d20651f0d6d1a49dbaf43f27839b8f9f01573
yousofhajhasan <youhajhasan@gmail.com>
2024-10-19 11:10:35 +0300

    Example6 Update

--- Example6.py
+++ Example6.py
@@ -1,4 +1,4 @@
-import time
+ import time
 import math
 import multiprocessing
 import sys
@@ -17,7 +17,7 @@ def process_camera(camera, shared_data, write, threshold, display, iou, draw, tr
 def send_push_notification(message):
     url = 'https://api.pushover.net/1/messages.json'
     payload = {
-        'token': 'tokenID',  # Replace with your Pushover application token
+        'token': 'TokenID',  # Replace with your Pushover application token
         'user': 'UserID',    # Replace with your Pushover user key
         'message': message          # The message you want to send
     }
@@ -27,37 +27,44 @@ def send_push_notification(message):
 def monitor_shared_data(shared_data):
     notified_outside = False  # Flag to track notification for "Right Now Outside"
     notified_entered = False  # Flag to track notification for "Entered The House"
+    
 
     last_notified_left_to_right = 0  # Track last notified count for left to right
     last_notified_right_to_left = 0  # Track last notified count for right to left
+    last_left_the_house = 0
 
     while True:
         current_left_to_right = shared_data.get("Left_to_right", 0)
         current_right_to_left = shared_data.get("Right_to_left", 0)
+        current_leave = shared_data.get("Left The House", 0)
 
+        # Someone left the house
+        if current_leave > last_left_the_house:
+            send_push_notification("Someone has left the house.")
+            last_left_the_house = current_leave
 
         if shared_data.get("Right Now Outside", 0) > 5 and not notified_outside:
             send_push_notification("More than 5 people are currently outside the house. Please check the situation.")
-            notified_outside = True
+            notified_outside = True  # Set the flag to True after sending notification
         elif shared_data.get("Right Now Outside", 0) <= 5:
-            notified_outside = False
+            notified_outside = False  # Reset the flag when the condition no longer holds
 
-        if shared_data.get("Entered The House", 0) > 5 and not notified_entered:
-            send_push_notification("More than 5 people have entered the house. This might be unusual.")
-            notified_entered = True  
+        if shared_data.get("Entered The House", 0) > 10 and not notified_entered:
+            send_push_notification("More than 10 people have entered the house. This might be unusual.")
+            notified_entered = True  # Set the flag to True after sending notification
 
-        elif shared_data.get("Entered The House", 0) <= 5:
-            notified_entered = False  
+        elif shared_data.get("Entered The House", 0) <= 10:
+            notified_entered = False  # Reset the flag when the condition no longer holds
 
-        if current_right_to_left // 2 > last_notified_right_to_left // 2:
-            send_push_notification("A new 10 cars have moved from right to left.")
-            last_notified_right_to_left = current_right_to_left  
+        if current_right_to_left // 5 > last_notified_right_to_left // 5:
+            send_push_notification("A new 5 cars have moved from right to left.")
+            last_notified_right_to_left = current_right_to_left  # Update last notified count
         
-        if current_left_to_right // 2 > last_notified_left_to_right // 2:
-            send_push_notification("A new 10 cars have moved from left to right.")
-            last_notified_left_to_right = current_left_to_right  
+        if current_left_to_right // 5 > last_notified_left_to_right // 5:
+            send_push_notification("A new 5 cars have moved from left to right.")
+            last_notified_left_to_right = current_left_to_right  # Update last notified count
         
-        time.sleep(0.2) # Check every 0.2 seconds
+        time.sleep(0.2)  # Poll every half second
 
 
 def json_serializable(data):
@@ -114,6 +121,7 @@ if __name__ == "__main__":
         pool.close()
         pool.join()
 
+        # Terminate the monitoring process
         monitor_process.terminate()
         monitor_process.join()
 
